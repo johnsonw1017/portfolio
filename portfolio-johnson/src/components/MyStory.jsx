ae752d184c778e4c146878037368deacd84a4737
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-10-12 22:35:53 +1000

    feat: my story section completed with placeholder text

--- portfolio-johnson/src/components/MyStory.jsx
+++ portfolio-johnson/src/components/MyStory.jsx
@@ -1,14 +1,29 @@
-import {
-  Container
-} from "@mui/material";
-
+import { Box, Container, Typography } from "@mui/material";
+import pallete from "../pallete";
 
 function MyStory() {
   return (
     <>
-      <Container maxWidth="md">My Story</Container>
+      <div style={{ backgroundColor: pallete.caroline_blue }}>
+        <Container maxWidth="md">
+          <Box className="box-content">
+            <Typography sx={{fontWeight: "700", fontSize: "30px", marginTop: 2}}>
+              My Story
+            </Typography>
+            <Typography sx={{fontWeight: "400", margin: 2}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+          </Box>
+        </Container>
+      </div>
     </>
-  )
+  );
 }
 
-export default MyStory
+export default MyStory;
