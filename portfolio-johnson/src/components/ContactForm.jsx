dec2ec7fa9e2f128aabdbd40572d5be82ed0a99e
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-10-13 21:35:45 +1000

    feat: contact form functionality implemented with emailjs

--- portfolio-johnson/src/components/ContactForm.jsx
+++ portfolio-johnson/src/components/ContactForm.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   Container,
   Box,
@@ -8,8 +9,95 @@ import {
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import pallete from "../pallete";
+import validate from "validate.js";
+import emailjs from "@emailjs/browser";
+
+const USER_ID = import.meta.env.VITE_EMAILJS_USERID;
+const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATEID;
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICEID;
+const schema = {
+  name: {
+    presence: { allowEmpty: false, message: "is required" },
+    length: {
+      maximum: 128,
+    },
+  },
+  phone: {
+    presence: { allowEmpty: true, message: "optional" },
+    length: {
+      maximum: 128,
+    },
+  },
+  email: {
+    presence: { allowEmpty: false, message: "is required" },
+    email: true,
+    length: {
+      maximum: 300,
+    },
+  },
+  message: {
+    presence: { allowEmpty: false, message: "is required" },
+    length: {
+      maximum: 1000,
+    },
+  },
+};
 
 function ContactForm() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((res) => console.log("SUCCESS!", res.status, res.text))
+      .catch((error) => console.log("FAILED...", error));
+
+    setFormState((formState) => ({
+      ...formState,
+      isValid: false,
+      values: {},
+      touched: {},
+      errors: {},
+    }));
+  };
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {},
+  });
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState((formState) => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+
+  const handleChange = (e) => {
+    e.persist();
+
+    setFormState((formState) => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [e.target.name]:
+          e.target.type === "checkbox" ? e.target.checked : e.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [e.target.name]: true,
+      },
+    }));
+  };
+
+  const hasError = (field) =>
+    formState.touched[field] && formState.errors[field] ? true : false;
+
   return (
     <section id="contact">
       <div style={{ backgroundColor: pallete.charcoal }}>
@@ -33,58 +121,115 @@ function ContactForm() {
             </Typography>
           </Box>
           <Box className="box-content">
-            <Grid container spacing={2} sx={{width: {xs: "250px", sm: "480px"}, justifyContent:"center"}} >
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Name"
-                  variant="filled"
-                  sx={{
-                    backgroundColor: pallete.whitegray,
-                    borderRadius: 2,
-                    width: "100%",
-                  }}
-                />
+            <form headers="application/json" name="contact-form" onSubmit={sendEmail}>
+              <Grid
+                container
+                spacing={2}
+                sx={{
+                  width: { xs: "250px", sm: "480px" },
+                  justifyContent: "center",
+                }}
+              >
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Name"
+                    placeholder="Name"
+                    name="name"
+                    id="name"
+                    helperText={
+                      hasError("name") ? formState.errors.name[0] : null
+                    }
+                    type="text"
+                    value={formState.values.name || ""}
+                    onChange={handleChange}
+                    error={hasError("name")}
+                    variant="filled"
+                    fullWidth
+                    sx={{
+                      backgroundColor: pallete.whitegray,
+                      borderRadius: 2,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Phone"
+                    placeholder="Phone"
+                    name="phone"
+                    id="phone"
+                    helperText={
+                      hasError("phone") ? formState.errors.phone[0] : null
+                    }
+                    type="text"
+                    value={formState.values.phone || ""}
+                    onChange={handleChange}
+                    error={hasError("phone")}
+                    variant="filled"
+                    fullWidth
+                    sx={{
+                      backgroundColor: pallete.whitegray,
+                      borderRadius: 2,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Email"
+                    variant="filled"
+                    placeholder="Email"
+                    name="email"
+                    id="email"
+                    helperText={
+                      hasError("email") ? formState.errors.email[0] : null
+                    }
+                    type="email"
+                    value={formState.values.email || ""}
+                    onChange={handleChange}
+                    error={hasError("email")}
+                    fullWidth
+                    sx={{
+                      backgroundColor: pallete.whitegray,
+                      borderRadius: 2,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Message"
+                    variant="filled"
+                    placeholder="Message"
+                    name="message"
+                    id="message"
+                    helperText={
+                      hasError("message") ? formState.errors.message[0] : null
+                    }
+                    type="text"
+                    value={formState.values.message || ""}
+                    onChange={handleChange}
+                    error={hasError("message")}
+                    fullWidth
+                    multiline
+                    rows={4}
+                    sx={{
+                      marginBottom: 2,
+                      backgroundColor: pallete.whitegray,
+                      borderRadius: 2,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    color="lapis"
+                    type="submit"
+                    disabled={!formState.isValid}
+                  >
+                    Send
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Phone"
-                  variant="filled"
-                  sx={{
-                    backgroundColor: pallete.whitegray,
-                    borderRadius: 2,
-                    width: "100%",
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Email"
-                  variant="filled"
-                  sx={{
-                    backgroundColor: pallete.whitegray,
-                    borderRadius: 2,
-                    width: "100%",
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Message"
-                  variant="filled"
-                  multiline
-                  rows={4}
-                  sx={{
-                    marginBottom: 2,
-                    backgroundColor: pallete.whitegray,
-                    borderRadius: 2,
-                    width: "100%",
-                  }}
-                />
-              </Grid>
-            </Grid>
-            <Button variant="contained" endIcon={<SendIcon />} color="lapis">
-              Send
-            </Button>
+            </form>
           </Box>
           <Box className="box-content">
             <Typography
@@ -95,7 +240,7 @@ function ContactForm() {
           </Box>
         </Container>
       </div>
-    </ section>
+    </section>
   );
 }
 
